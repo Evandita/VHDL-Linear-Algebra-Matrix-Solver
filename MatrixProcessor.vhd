a1cf901a46d8c18ea76edefd4b597dd92b5f86f8
Evandita <evanditaw@gmail.com>
2023-12-16 12:32:01 +0700

    Penambahan Komponen Utama Matrix Processor

---
+++ MatrixProcessor.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity MatrixProcessor is
+    port (
+        
+    );
+end entity MatrixProcessor;
+
+architecture rtl of MatrixProcessor is
+    
+begin
+    
+    
+    
+end architecture rtl;
